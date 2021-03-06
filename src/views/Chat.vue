bf48be8c836f517ee545acfe0671c5eb9c15295e
Patrick M <tatater56@gmail.com>
2021-03-06 00:38:49 -0500

    remove Hello World, added views skeletons

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
+  <div class="chat">
+    <h1>Chat</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
+  name: "Chat",
   components: {}
 };
 </script>
