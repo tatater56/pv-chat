e601c65e79aa261fc529ae68e790bfbb0cd8fd04
Patrick M <tatater56@gmail.com>
2021-03-07 12:28:02 -0500

    add vue-scroll-chat dependency

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import VueChatScroll from "vue-chat-scroll";
 
 import Home from "@/views/Home.vue";
 import Chat from "@/views/Chat.vue";
 
 Vue.use(VueRouter);
+Vue.use(VueChatScroll);
 
 const routes = [
   {
