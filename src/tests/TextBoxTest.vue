0c5b6839e4eb665e1346d6238e1e19a160f7ac2a
Patrick M <tatater56@gmail.com>
2021-03-06 21:39:54 -0500

    seperate TextBoxTest

---
+++ src/tests/TextBoxTest.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="TextBoxTest">
+    <p class="header">TextBoxTest {{ value }}</p>
+    <text-box placeholder="text" @submitted="submitted" @changed="changed" />
+  </div>
+</template>
+
+<script>
+import TextBox from "@/components/TextBox";
+
+export default {
+  name: "TextBoxTest",
+  components: {
+    TextBox
+  },
+  data() {
+    return {
+      value: ""
+    };
+  },
+  methods: {
+    submitted(value) {
+      this.value = value;
+      alert(this.value);
+    },
+    changed(value) {
+      this.value = value;
+    }
+  }
+};
+</script>
