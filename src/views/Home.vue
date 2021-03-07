196ff207966369fbbece2fbe4235abe7999dbfba
Patrick M <tatater56@gmail.com>
2021-03-06 22:27:55 -0500

    home view skeleton

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="home">
-    <p class="header">PV Chat {{ value }}</p>
-    <text-box placeholder="text" @submitted="submitted" @changed="changed" />
+    <p class="header">PV Chat</p>
+    <text-box placeholder="enter a nickname" @changed="nicknameChanged" />
+    <div class="spacer"></div>
+    <p class="subheader">tell your friend your id: {{ your_id }}</p>
+    <div class="spacer"></div>
+    <p><i>or...</i></p>
+    <div class="spacer"></div>
+    <text-box
+      placeholder="connect with your friend's id"
+      @changed="friendIdChanged"
+    />
   </div>
 </template>
 
@@ -15,17 +24,24 @@ export default {
   },
   data() {
     return {
-      value: ""
+      nickname: "",
+      your_id: "...",
+      friend_id: ""
     };
   },
   methods: {
-    submitted(value) {
-      this.value = value;
-      alert(this.value);
+    nicknameChanged(nickname) {
+      this.nickname = nickname;
     },
-    changed(value) {
-      this.value = value;
+    friendIdChanged(friend_id) {
+      this.friend_id = friend_id;
     }
   }
 };
 </script>
+
+<style scoped>
+.spacer {
+  margin-bottom: 5rem;
+}
+</style>
